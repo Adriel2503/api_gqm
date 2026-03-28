2cd14a13bdeabdd9195c9e0343ffbe83470676a9
Dante <ariel.frias.r@uni.pe>
2026-03-28 08:14:03 -0500

    User app deactivate

--- main.py
+++ main.py
@@ -62,4 +62,4 @@ def buscar(request: BuscarRequest):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
